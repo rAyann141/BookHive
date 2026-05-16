9119db5b920947ec1788b18167a5d0cae6283759
BookHive Developer <labor@JERU>
2026-05-16 23:56:06 +0800

    feat: enable network access for dev environment and update CORS policy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["pg"], // Example for database drivers if needed in server actions
+  allowedDevOrigins: ["192.168.1.28", "localhost:3000"],
+  devIndicators: {
+    position: "bottom-left",
+  },
 };
 
 export default nextConfig;
